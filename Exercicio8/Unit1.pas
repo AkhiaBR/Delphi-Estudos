f769c419e3aed587c2b793335f557b26c480396b
Fernando <fernandooogoncalves@gmail.com>
2025-12-07 20:43:51 -0300

    exercicio 8 finalizado

---
+++ Exercicio8/Unit1.pas
@@ -0,0 +1,52 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, RLReport, FireDAC.Stan.Intf,
+  FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf,
+  FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys,
+  FireDAC.Phys.MySQL, FireDAC.Phys.MySQLDef, FireDAC.VCLUI.Wait, Data.DB,
+  FireDAC.Comp.Client, FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf,
+  FireDAC.DApt, FireDAC.Comp.DataSet, Vcl.StdCtrls, Data.Win.ADODB;
+
+type
+  TForm1 = class(TForm)
+    Conexao: TFDConnection;
+    DriverMySQL: TFDPhysMySQLDriverLink;
+    RLReport1: TRLReport;
+    DBFonte: TDataSource;
+    RLBCabecalho: TRLBand;
+    RLBTitulo: TRLBand;
+    RLBColunas: TRLBand;
+    RLBDetail: TRLBand;
+    RLSystemInfo1: TRLSystemInfo;
+    RLLabel1: TRLLabel;
+    RLSystemInfo2: TRLSystemInfo;
+    RLLabel2: TRLLabel;
+    RLLabel3: TRLLabel;
+    RLLabel4: TRLLabel;
+    RLLabel5: TRLLabel;
+    RLLabel6: TRLLabel;
+    RLLabel7: TRLLabel;
+    RLDBText1: TRLDBText;
+    RLDBText2: TRLDBText;
+    RLDBText3: TRLDBText;
+    RLDBText4: TRLDBText;
+    RLDBText5: TRLDBText;
+    FDQuery1: TFDQuery;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
